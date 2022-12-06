2dfdf12d7853fdb76b776adf16bdc2fe78644b66
MATZOURANIS Sevastos <sevastos.matzouranis@sword-group.com>
2022-12-06 10:31:52 +0200

    initial commit

---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+export interface User {
+  name: string;
+  surname: string;
+  gender: string;
+  age: string;
+  phone: string;
+  mobile: string;
+  email: string;
+  metrics: Metrics;
+}
+
+export interface Metrics {
+  weight: string;
+  heigth: string;
+  bmi: string;
+  bodyFat: string;
+  nonFatMass: string;
+  vf: string;
+  w: string;
+  biseps: string;
+  chest: string;
+  waist: string;
+  belly: string;
+  pelvis: string;
+  thigh: string;
+  calves: string;
+}
